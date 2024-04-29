7a38082a99c0200b8366e69074c4e29c3dc78359
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-04-29 14:04:18 +0900

    login form submit

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,6 +1,32 @@
 import {Link} from "react-router-dom";
+import {FormEvent, useState} from "react";
+import axios from 'axios';
 
 const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+
+        const payload = {
+            user: {
+                email: email,
+                password: password
+            }
+        }
+
+        try {
+            const response = await axios.post('localhost:8080/login', payload);
+            if (response.data) {
+                console.log('Success! Redirect to home page or set user data in state');
+            } else {
+                console.error('Failed to login');
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    }
+
     return (
         <div className="auth-page">
             <div className="container page">
@@ -15,12 +41,24 @@ const Login = () => {
                             <li>That email is already taken</li>
                         </ul>
 
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <fieldset className="form-group">
-                                <input className="form-control form-control-lg" type="text" placeholder="Email"/>
+                                <input
+                                    className="form-control form-control-lg"
+                                    type="text"
+                                    placeholder="Email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
                             </fieldset>
                             <fieldset className="form-group">
-                                <input className="form-control form-control-lg" type="password" placeholder="Password"/>
+                                <input
+                                    className="form-control form-control-lg"
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
                             </fieldset>
                             <button className="btn btn-lg btn-primary pull-xs-right">Sign in</button>
                         </form>
